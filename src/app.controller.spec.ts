01769a85454e584990af25f2453ff241e2fd077d
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2024-03-03 17:07:14 +0530

    Added spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello From Url Shortener!"', () => {
+      expect(appController.getHello()).toBe('Hello From Url Shortener!');
     });
   });
 });
